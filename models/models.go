eb434e7187127801a7920f560f9767b1ffdcc6ff
Glenn <glennchiang24@gmail.com>
2024-12-10 12:55:29 +0800

    Add Topic model and define relations between User, Post, Comment and Topic

--- models/models.go
+++ models/models.go
@@ -4,18 +4,27 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `json:"username"`
+	Username string `gorm:"uniqueIndex" json:"username"`
 }
 
 type Post struct {
 	gorm.Model
 	Title string `json:"title"`
 	Content string `json:"content"`
-	AuthorID string `json:"authorId"`
+	AuthorID uint `json:"authorId"`
+	Author User `gorm:"constraint:OnDelete:SET NULL;"`
+	Topics []*Topic `gorm:"many2many:post_topics"`
 }
 
 type Comment struct {
 	gorm.Model
 	Content string `json:"content"`
-	PostID string `json:"postId"`
+	PostID uint `json:"postId"`
+	Post Post `gorm:"constraint:OnDelete:SET NULL;"`
+}
+
+type Topic struct {
+	ID uint `json:"id"`
+	Name string `gorm:"uniqueIndex" json:"name"`
+	Posts []*Post `gorm:"many2many:post_topics;"`
 }
