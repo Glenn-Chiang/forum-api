61309f07a1a48ff5439b13a36d6a433fc8656568
Glenn <glennchiang24@gmail.com>
2025-01-24 12:12:51 +0800

    Update Post and Comment models to have onDelete:CASCADE constraint for associated votes

--- internal/models/models.go
+++ internal/models/models.go
@@ -27,7 +27,7 @@ type Post struct {
 	// Implicitly create a many2many join table between posts and topics. When a post is deleted, the post_topic record in the join table is deleted. The associated topics themselves are not deleted.
 	Topics []Topic `json:"topics" gorm:"many2many:post_topics;constraint:OnDelete:CASCADE;"`
 	// Array of votes associated with this post. Not included in json.
-	Votes []PostVote `json:"-"`
+	Votes []PostVote `json:"-" gorm:"constraint:OnDelete:CASCADE;"` // When this post is deleted, the associated comments are deleted.
 
 	// Upvotes minus downvotes
 	// Computed field, not included in database
@@ -58,15 +58,15 @@ type PostTagsUpdate struct {
 
 type Comment struct {
 	ID        uint      `json:"id"`
-	Content   string    `gorm:"not null" json:"content"`
+	Content   string    `json:"content" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	PostID    uint      `json:"post_id"` // By default, when the associated post is deleted, the comment remains but the post_id is set to null
+	PostID    uint      `json:"post_id" gorm:"constraint:OnDelete:SET NULL;" ` // When the associated post is deleted, the comment remains but the post_id is set to null
 	AuthorID  uint      `json:"author_id"`
-	Author    User      `gorm:"constraint:OnDelete:SET NULL;" json:"author"` // When the associated user is deleted, set the author field to null
+	Author    User      `json:"author" gorm:"constraint:OnDelete:SET NULL;"` // When the associated user is deleted, set the author field to null
 
 	// Array of votes associated with this comment. Not included in json.
-	Votes []CommentVote `json:"-"`
+	Votes []CommentVote `json:"-" gorm:"constraint:OnDelete:CASCADE;"` // When this comment is deleted, the associated votes are deleted.
 
 	// Upvotes minus downvotes
 	// Computed field, not included in database
@@ -91,17 +91,17 @@ type CommentUpdate struct {
 // Record for a user's vote for a post
 type PostVote struct {
 	// Composite primary key using post_id and user_id
-	PostID uint `json:"post_id" gorm:"primaryKey;autoIncrement:false" `
-	UserID uint `json:"user_id" gorm:"primaryKey;autoIncrement:false"`
+	PostID uint `json:"post_id" gorm:"primaryKey;autoIncrement:false;"`
+	UserID uint `json:"user_id" gorm:"primaryKey;autoIncrement:false;"`
 	Value  int  `json:"value" gorm:"not null"` //upvote: 1, downvote: -1
 }
 
 // Record for a user's vote for a comment
 type CommentVote struct {
 	// Composite primary key using comment_id and user_id
-	CommentID uint `json:"comment_id" gorm:"primaryKey;autoIncrement:false" `
-	UserID uint `json:"user_id" gorm:"primaryKey;autoIncrement:false"`
-	Value  int  `json:"value" gorm:"not null"` //upvote: 1, downvote: -1
+	CommentID uint `json:"comment_id" gorm:"primaryKey;autoIncrement:false;" `
+	UserID    uint `json:"user_id" gorm:"primaryKey;autoIncrement:false;"`
+	Value     int  `json:"value" gorm:"not null"` //upvote: 1, downvote: -1
 }
 
 // Request body for voting on a post or comment
